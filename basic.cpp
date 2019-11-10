be11c208f2abf9cf81c3882ada0d60a3cefc2d32
RASEL <rhrasel94@gmail.com>
2019-11-11 01:59:23 +0600

    this is basic input/output program in cpp

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int number ;
+      cout<<"Enter any input value : ";
+      cin>>number ;
+
+      cout<<"The number  is : " <<number<<endl ;
+
+
+
+}
+
