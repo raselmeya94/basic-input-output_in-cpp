ef3e8ebd98aaa4e20567d3d6ef72f6a8fb6a49ab
RaselMeya <55887770+RaselMeya@users.noreply.github.com>
2019-11-11 02:02:19 +0600

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main()
 {
       int number ;
+      
       cout<<"Enter any input value : ";
+      
       cin>>number ;
 
       cout<<"The number  is : " <<number<<endl ;
